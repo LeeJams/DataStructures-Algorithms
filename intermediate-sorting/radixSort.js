5473a08773e5b75efc7a6684ad9a2617fe2f4dcf
LeeJams <leejam208@gmail.com>
2022-07-05 18:19:35 +0900

    radixSort

---
+++ intermediate-sorting/radixSort.js
@@ -0,0 +1,18 @@
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(arr) {
+  let maxDigits = 0;
+  for (let i = 0; i < arr.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(arr[i]));
+  }
+  return maxDigits;
+}
+
+mostDigits([4, 1, 5112, 5, 123, 15, 16, 1, 45124, 1123]);
