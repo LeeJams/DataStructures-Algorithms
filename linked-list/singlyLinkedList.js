0251d67da4781771a1a2a7636fb8cf6d84dd8807
LeeJams <leejam208@gmail.com>
2022-07-12 18:01:18 +0900

    Singly Linked List

---
+++ linked-list/singlyLinkedList.js
@@ -0,0 +1,76 @@
+// piece of data - val
+//reference to next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (this.length) {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  traverse() {
+    let current = this.head;
+    while (current) {
+      console.log(current.val);
+      current = current.next;
+    }
+  }
+  pop() {
+    if (!this.length) {
+      return undefined;
+    }
+    let pre = this.head;
+    let cur = pre;
+
+    while (cur.next) {
+      pre = cur;
+      cur = pre.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return cur;
+  }
+}
+
+// var first = new Node("Hi")
+// first.next = new Node("there")
+// first.next.next = new Node("how")
+// first.next.next.next = new Node("are")
+// first.next.next.next.next = new Node("you")
+
+var list = new SinglyLinkedList();
+console.log(list.push("HELLO"));
+console.log(list.push("GOODBYE"));
+console.log(list.push(44));
+
+list.traverse();
+
+console.log("pop - ", list.pop());
+console.log("pop - ", list.pop());
+console.log("pop - ", list.pop());
+console.log("pop - ", list.pop());
+
+console.log(list);
